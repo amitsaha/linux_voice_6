beb5093547eb886267d89d771309fd9c4f1ac03a
Amit Saha <amitsaha.in@gmail.com>
2016-08-28 22:55:57 +1000

    Working ws client

--- chat_app/src/chat_app.cr
+++ chat_app/src/chat_app.cr
@@ -1,15 +1,22 @@
 require "kemal"
 
+clients = [] of HTTP::WebSocket
+
 get "/" do
   "Hello World"
 end
 
 ws "/socket" do |socket|
+
+  clients << socket
+
   socket.on_message do |message|
-    puts "Got: #{message}"
-    socket.send "Echo from server: #{message}"
+    clients.each {|socket| socket.send message}
   end
 
+  socket.on_close do
+    clients.delete socket
+  end
 end
 
 Kemal.run
--- ws_client.cr
+++ ws_client.cr
@@ -1,10 +1,20 @@
 require "http"
 ws = HTTP::WebSocket.new(URI.parse("ws://localhost:3000/socket"))
-
-
-while true
-  ws.send("Hello")
-  ws.on_message do |message|
-    puts "Got #{message} from server"
-  end
+# This fiber will run concurrently waiting for a line of input
+# and then writing to the socket
+spawn do
+    while true
+      msg = gets()
+      if msg
+        ws.send(msg)
+      end
+    end
 end
+
+# Display any message we get from the server
+ws.on_message do |message|
+    puts "Got: #{message}"
+end
+
+# Start the ws loop
+ws.run
