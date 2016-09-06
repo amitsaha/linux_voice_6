b31a60e69471aee7e655162771e7c4c19556889b
Amit Saha <amitsaha.in@gmail.com>
2016-09-06 13:51:54 +1000

    More spec tests

--- broadcaster/spec/broadcaster_spec.cr
+++ broadcaster/spec/broadcaster_spec.cr
@@ -1,14 +1,22 @@
 require "./spec_helper"
+require "http"
 
 describe Broadcaster do
-  # TODO: Write tests
+  # Start our kemal web application
   start
 
-  it "renders /" do
+  it "can call /" do
     get "/"
     response.body.should contain "Hello client"
   end
 
+  it "can talk to websocket /socket" do
+    server = "#{APP_URL}/socket"
+    ws = HTTP::WebSocket.new(URI.parse(server))
+    ws.send("Hello from client")
+ end
+
+  # Stop our web application
   stop
 
 end
